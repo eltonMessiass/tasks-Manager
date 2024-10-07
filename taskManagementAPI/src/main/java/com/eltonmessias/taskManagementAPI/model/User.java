94ffa9859f807787ce7f2b660622451c33002fb2
elton <eltonmessias10@gmail.com>
2024-10-08 00:06:48 +0200

    feat:user model

---
+++ taskManagementAPI/src/main/java/com/eltonmessias/taskManagementAPI/model/User.java
@@ -0,0 +1,18 @@
+package com.eltonmessias.taskManagementAPI.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String password;
+}
